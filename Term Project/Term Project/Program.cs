c0e80fb709ab6c882820945a7744c11fd444a124
Kurt85 <127762132+Kurt85@users.noreply.github.com>
2023-06-09 15:03:13 -0600

    Term Proejct Starter Blank File

---
+++ Term Project/Term Project/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Term_Project
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
